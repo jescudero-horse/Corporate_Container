db4a4b6751538513a2df5c2f4dac1d4052dcd9e6
jescudero-horse <jesus.escudero-extern@horse.tech>
2025-02-25 12:40:12 +0100

    Update film.js
    - Formulario con el número de picadas

--- src/public/script/film/film.js
+++ src/public/script/film/film.js
@@ -2594,6 +2594,19 @@ function anyadirEtapa(id) {
                     </select>
                 </div>
 
+                <!--Numero picadas -->
+                <div class="relative inline-block w-64 mb-4">
+                    <label for="numeroPicadas" class="block text-sm font-medium text-white mb-2">Número de picadas</label>
+                    <select id="numeroPicadas" name="numeroPicadas" class="block w-full pl-3 pr-10 py-2 text-base border-gray-600 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md bg-white text-white">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                    </select>
+                </div>
+
                 <!-- Botón obtener información -->
                 <div class="mt-6">
                     <button type="submit" class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-500 transition duration-300">Obtener información <i class="bi bi-info-circle-fill"></i></button>
